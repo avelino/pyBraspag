0ec9b3c3b776d82cf10acde5152795cce4a188d8
Dilan Nery Lopes <dnerylopes@gmail.com>
2010-12-31 11:05:17 -0300

    Estruturação

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- conding: utf-8 -*-
+
+from main import *
---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+class Braspag(object):
+	def test():
+		return "test"
